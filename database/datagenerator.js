5ab883df726050fb553f4005d9c6aadde37993cc
basti <bastipacudan@gmail.com>
2019-04-22 14:35:36 -0700

    fixed indentation

--- database/datagenerator.js
+++ database/datagenerator.js
@@ -25,27 +25,27 @@ const generateData = async (callback) => {
                       ${ faker.random.number({min: 1, max: 999})},${faker.random.number({min: 2, max: 5, precision: 0.01})},
                       ${faker.random.number({min: 3, max: 10})},${faker.random.number({min: 11, max: 30})}\n`
         // check if it write is unsuccessful
-        if(!data.write(post)) {
+        if (!data.write(post)) {
             // call the drain function to clear memory
             await new Promise(resolve => data.once('drain', resolve));
         }
         // write random image info for the current listing id on /database/files/images.csv
         for (let idx = 0; idx < randPhotoNum; idx++) {
-            if(!images.write(`${idx},${id},${faker.image.food()},${faker.random.word() + faker.date.past()}\n`)) {
+            if (!images.write(`${idx},${id},${faker.image.food()},${faker.random.word() + faker.date.past()}\n`)) {
                 await new Promise(resolve => images.once('drain',resolve))
             }
         } 
         // write random tags info for the current listing id on /database/files/tags.csv
-        for(let idx = 0; idx < randTagNum + 5; idx++) {
-            if(!tags.write(`${idx},${id},${faker.random.word()}\n`)) {
+        for (let idx = 0; idx < randTagNum + 5; idx++) {
+            if (!tags.write(`${idx},${id},${faker.random.word()}\n`)) {
                 await new Promise(resolve => tags.once('drain',resolve))
             }
         }
         // write random types info for the current listing id on /database/files/types.csv
-        for(let idx = 0; idx < randCuiNum; idx++) {
+        for (let idx = 0; idx < randCuiNum; idx++) {
           // choose a random index from cuisine array
           const randCuisineIndex = Math.floor(Math.random() * cuisine.length);
-          if(!types.write(`${idx},${id},${cuisine[randCuisineIndex]}\n`)) {
+          if (!types.write(`${idx},${id},${cuisine[randCuisineIndex]}\n`)) {
             await new Promise(resolve => types.once('drain',resolve))
           }
         }
