c1afb7c1ef844f7972c82049818a45b493fec2e2
basti <bastipacudan@gmail.com>
2019-04-22 20:29:51 -0700

    refactored datagenerator script

--- database/datagenerator.js
+++ database/datagenerator.js
@@ -1,8 +1,8 @@
 const faker = require('faker');
-const data = require('fs').createWriteStream('database/files/data.csv');
-const images = require('fs').createWriteStream('database/files/images.csv');
-const tags = require('fs').createWriteStream('database/files/tags.csv');
-const types = require('fs').createWriteStream('database/files/types.csv');
+const data = require('fs').createWriteStream('database/sample/data.csv');
+const images = require('fs').createWriteStream('database/sample/images.csv');
+const tags = require('fs').createWriteStream('database/sample/tags.csv');
+const types = require('fs').createWriteStream('database/sample/types.csv');
 
 const generateData = async (callback) => {
     
@@ -10,20 +10,18 @@ const generateData = async (callback) => {
                     'Vietnamese', 'Taiwanese', 'Brazilian', 'Italian', 'Martian', 'Connecticotian',
                      'Indian', 'Jamaican', 'Seafood', 'Ethiopian', 'Mediterranean', 'Californian'];
     // write headers for every csv file
-    data.write('id,name,description,reviews,priceStart,priceEnd\n');
-    images.write('id,listingId,imgUrl,caption\n');
-    tags.write('id,listingId,tag\n');
-    types.write('id,listingId,type\n');
+    data.write('id|name|description|reviews|priceStart|priceEnd\n');
+    images.write('listingId|imgUrl|caption\n');
+    tags.write('listingId|tag\n');
+    types.write('listingId|type\n');
     // create 10m records 
-    for(let id = 1; id <= 1e7; id++) {
+    for(let id = 1; id <= 5; id++) {
         // generate random number for photo, tags and cuisines
         const randPhotoNum = Math.floor(Math.random() * 35 + 10);
         const randTagNum = Math.floor(Math.random() * 10 + 3);
         const randCuiNum = Math.floor(Math.random() * 2 + 1);
         // create a random data with faker
-        const post = `${id},${faker.random.word()},${faker.lorem.paragraphs(3)},
-                      ${ faker.random.number({min: 1, max: 999})},${faker.random.number({min: 2, max: 5, precision: 0.01})},
-                      ${faker.random.number({min: 3, max: 10})},${faker.random.number({min: 11, max: 30})}\n`
+        const post = `${id}|${faker.random.word()}|${JSON.stringify(faker.lorem.paragraphs(3))}|${faker.random.number({min: 1, max: 999})}|${faker.random.number({min: 2, max: 5, precision: 0.01})}|${faker.random.number({min: 3, max: 10})}\n`
         // check if it write is unsuccessful
         if (!data.write(post)) {
             // call the drain function to clear memory
@@ -31,13 +29,13 @@ const generateData = async (callback) => {
         }
         // write random image info for the current listing id on /database/files/images.csv
         for (let idx = 0; idx < randPhotoNum; idx++) {
-            if (!images.write(`${idx},${id},${faker.image.food()},${faker.random.word() + faker.date.past()}\n`)) {
+            if (!images.write(`${id}|${faker.image.food()},${faker.random.word() + faker.date.past()}\n`)) {
                 await new Promise(resolve => images.once('drain',resolve))
             }
         } 
         // write random tags info for the current listing id on /database/files/tags.csv
         for (let idx = 0; idx < randTagNum + 5; idx++) {
-            if (!tags.write(`${idx},${id},${faker.random.word()}\n`)) {
+            if (!tags.write(`${id}|${faker.random.word()}\n`)) {
                 await new Promise(resolve => tags.once('drain',resolve))
             }
         }
@@ -45,7 +43,7 @@ const generateData = async (callback) => {
         for (let idx = 0; idx < randCuiNum; idx++) {
           // choose a random index from cuisine array
           const randCuisineIndex = Math.floor(Math.random() * cuisine.length);
-          if (!types.write(`${idx},${id},${cuisine[randCuisineIndex]}\n`)) {
+          if (!types.write(`${id}|${cuisine[randCuisineIndex]}\n`)) {
             await new Promise(resolve => types.once('drain',resolve))
           }
         }
